0e6365ddd7efc8f1af146f7fd4a7df9b3615940c
Jusong Yu <jusong.yeu@gmail.com>
2023-02-20 00:51:05 +0100

    Set qwert layer toggle by FN3

--- keyboards/keychron/v4/ansi/keymaps/qdvorak/keymap.c
+++ keyboards/keychron/v4/ansi/keymaps/qdvorak/keymap.c
@@ -21,6 +21,7 @@
 enum layers {
     MAC_BASE,
     WIN_BASE,
+    QWERT,
     _FN1,
     _FN2,
     _FN3
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_QUOT,  KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,     KC_SLSH,  KC_EQL,   KC_BSLS,
         KC_LCTL,  KC_A,     KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,     KC_MINS,             KC_ENT,
         KC_LSFT,            KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,        KC_Z,            KC_RSFT,
-        MO(_FN1), KC_LOPT,  KC_LCMD,                            KC_SPC,                             KC_RCMD,  MO(_FN1), MO(_FN3), KC_CAPS
+        MO(_FN1), KC_LOPT,  KC_LCMD,                            KC_SPC,                             KC_RCMD,  MO(_FN1), MO(QWERT), KC_CAPS
     ),
 
     [WIN_BASE] = LAYOUT_ansi_61(
@@ -59,6 +60,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
         KC_LCTL, KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN2), MO(_FN3), KC_RCTL
     ),
+/* Keymap 2: QWERT layer
+ *
+ * ,------------------------------------------------------------------------------------------------------.
+ * |        |      |      |      |      |      |      |      |      |      |      |      |      |         |
+ * |--------+------+------+------+------+------+------|------+------+------+------+------+----------------|
+ * |        |   Q  |  W   |  E   |  R   |  T   |  Y   |  U   |   I  |  O   |   P  |   [  |  ]   |    \    |
+ * |--------+------+------+------+------+------|---------------+------+------+------+------+--------------|
+ * |        |   A  |  S   |  D   |  F   |  G   |  H   |  J   |   K  |   L  |   ;  |   '  |                |
+ * |--------+------+------+------+------+------|-------------|------+------+------+------+------+---------|
+ * |               |  Z   |  X   |  C   |  V   |  B   |  N   |   M  |   ,  |   .  |   /  |                |
+ * `--------+------+------+------+------+------------------+------+------+------+------+------------------'
+ * |        |      |      |                                                |      |      |       |        | 
+ * `------------------------------------'-----------------------------------------------------------------.
+ */
+    [QWERT] = LAYOUT_ansi_61(
+        _______, _______, _______,  _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,
+        _______,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        _______,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,           _______,
+        _______,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,           _______,
+        _______, _______, _______,                            _______,                              _______, _______, _______, _______
+    ), 
 /* Keymap 2: FN1 layer
  *
  * ,------------------------------------------------------------------------------------------------------.
